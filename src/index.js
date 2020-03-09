4e18d15a011a3a3a4aee70723b3f28b0357e811d
Vadim <v.dudko93@gmail.com>
2020-03-09 22:56:15 +0300

    feat: human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function toReadable (number) {
-  
-}
+    const zeroNine = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const tenNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const twentyNinety = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let result = [];
+
+    if (number === 0){
+        return "zero";
+    }
+    if (number >= 100){
+        result.push(zeroNine[Math.floor(number / 100)]);
+        result.push("hundred");
+        number %= 100;
+    }
+    if (number >= 20){
+        result.push(twentyNinety[Math.floor(number / 10) - 2]);
+        number %= 10;
+    }
+    if (number >= 10){
+        result.push(tenNineteen[(number % 10)]);
+        number = 0;
+    }
+    if (number > 0){
+        result.push(zeroNine[number]);
+    }
+    return result.join(' ').trim();
+};
